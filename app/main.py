842220040aef2a85c11acb29d526244520d4f77f
tamses4 <tamar.ondoa@icloud.com>
2025-11-02 19:40:32 -0800

    API

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from weasyprint import HTML
 from datetime import datetime
-import tempfile
+import tempfile, tabnanny
 
 app = FastAPI(title="Smart CV Generator")
 # configuration templates & static
